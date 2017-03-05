1b968f937e775c9c9f64157d021516a693bff33d
offsky <petrodzher@gmail.com>
2017-03-05 13:19:02 +0200

    config changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'mongoUrl': 'mongodb://heroku_twvh2p50:gogolbordello12@ds117830.mlab.com:17830/heroku_twvh2p50',
+    'mongoUrl': 'mongodb://heroku_twvh2p50:romaroma12@ds117830.mlab.com:17830/heroku_twvh2p50',
     'secretKey': "Who knows what",
     'adminCode': 'KIDDS',
     'testResultStorageTimeInDays': 31,
